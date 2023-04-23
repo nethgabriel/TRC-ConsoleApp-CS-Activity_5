4a560bf736488be53fca519f244699861fc16504
Kenneth Tapang <kennethgabtapang@gmail.com>
2023-04-23 15:47:00 +0800

    Add project files.

---
+++ Activity_5/Program.cs
@@ -0,0 +1,31 @@
+﻿//Read and Write Text Line
+
+namespace Activity_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            StreamReader sr = new StreamReader("C:\\Users\\KENNETH GABRIEL\\source\\repos\\StreamReader.txt");
+
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("C:\\Users\\KENNETH GABRIEL\\source\\repos\\StreamReader.txt");
+
+            sw.WriteLine("Hi World!!");
+            sw.WriteLine("From Activity_5");
+
+            sw.Close();
+        }
+    }
+}
